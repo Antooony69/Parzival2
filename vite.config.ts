8f7082dbdf9ee5d528d742b691941c5c09569763
Antony <132155404+Parzivalmmo@users.noreply.github.com>
2026-08-06 16:47:55 -0500

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Nuevoe4elite/', // Tiene que ser exactamente así, respetando la N mayúscula
+  base: './',
 })

0ba4c94d4cbf1ca8fd2ac2c65659d17d522c9a7b
Antony <132155404+Parzivalmmo@users.noreply.github.com>
2026-08-06 17:05:26 -0500

    Update base path in vite.config.ts
    Change base path in Vite configuration to match repository name.

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/Nuevoe4elite/', // 👈 Debe ser exactamente el nombre de tu repositorio
 })
